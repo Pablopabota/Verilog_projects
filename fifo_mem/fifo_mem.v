5960fa420705608afe04e4ca8b70f496c0151bb4
Pablopabota <pablis_94@hotmail.com>
2021-08-09 22:23:37 -0300

    avances en memoria fifo

--- fifo_mem/fifo_mem.v
+++ fifo_mem/fifo_mem.v
@@ -6,30 +6,56 @@ module fifo_counter (
     pndng,          //  Indica que hay datos sin leer
     pointer_in,     //  Puntero a donde guardar
     pointer_out,    //  Puntero a donde leer
+    rst
 );
     // defino la profundidad del fifo que maneja
     parameter depth = 8;
 
     // Señales de entrada
-    input clk, push, pop;
+    input clk, push, pop, rst;
 
     // Señales de salida
     output full, pndng;
-    output [depth:0] pointer_in;
-    output [depth:0] pointer_out;
+    output reg[depth:0] pointer_in;
+    output reg[depth:0] pointer_out;
 
     //  Señales internas
-    
 
+always @(posedge clk or rst) begin
+
+    if(rst == 1) begin
+        pointer_in <= 0;
+        pointer_out <= 0;
+        full <= 0;
+        pndng <= 0;
+    end
+
+    if(push == 1) begin
+        pointer_in =  pointer_in + 1;
+    end
+    else if (pop == 1) begin
+        pointer_out = pointer_out + 1;
+    end
+
+
+end
 endmodule
 
 module muxnm (
-    cntrl,  // Entradas de control
+    ctrl,  // Entradas de control
     in_n,   // Entrada de N Bits
     out_nm  // M salidas de N bits
 );
     parameter bits = 8;     // Parametro para largo del registro
     parameter depth = 4;    // Parametro para cantidad de registros
+
+    input [$clog2(depth)-1:0] ctrl; // Si debo demux DEPTH señales preciso Log_2(DEPTH) bits de control
+    input [bits-1:0] out_nm [depth-1:0];
+    output [bits-1:0] in_n;
+
+    always @(*) begin
+        out_nm[ctrl] = in_n;
+    end
 
 endmodule
 
@@ -46,7 +72,7 @@ module demuxnm (
     output [bits-1:0] out_n;
 
     always @(*) begin
-        
+        out_n = in_nm[ctrl];
     end
 
 endmodule
@@ -67,5 +93,38 @@ module d_ff_n (
     always @(posedge clk) begin // Siempre que recibo un flanco de clk
         Dout_n = Din_n; // La salida pasa a ser la entrada
     end
+
+endmodule
+
+module fifo (
+    clk,
+    Din,
+    Dout,
+    push,
+    pop,
+    full,
+    pndng,
+    rst
+);
+    parameter bits = 8;
+    parameter depth = 4;
+
+    input clk, push, pop, rst;
+    input Din;
+
+    output full, pndng;
+    output Dout;
+
+    reg count;
+
+    wire pointer_in, pointer_out;
+    wire out_nm_mux;
+    wire in_nm_demux;
+
+    fifo_counter U1(.push(push), .pop(pop), .pointer_in(pointer_in), .pointer_out(pointer_out));
+    muxnm U2(.ctrl(pointer_in), .in_n(Din), .out_nm(out_nm_mux));
+    d_ff_n U3(clk, .Din(out_nm_mux), .Dout(in_nm_demux));
+    demuxnm U4(.ctrl(pointer_out), .in_nm(in_nm_demux), .out_n(Dout));
+
 
 endmodule
