57e5895146c87b324ada14717962f3820f136d26
Pablopabota <pablis_94@hotmail.com>
2021-08-30 21:27:16 -0300

    update

--- Curso_DDA/unit1/shiftreg.v
+++ Curso_DDA/unit1/shiftreg.v
@@ -1,8 +1,11 @@
-module shiftreg (
-    output [3:0] o_led,
+module shiftreg #(
+    NB_LEDS = 4
+)(
+    output [NB_LEDS-1:0] o_led,
     input i_valid,
     input i_reset,
     input clock
 );
+    reg [NB_LEDS-1:0] shiftreg;
     
 endmodule
