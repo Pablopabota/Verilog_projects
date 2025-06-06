90cfb3519cc31ff7e96c275ba0defab17c41e20c
pablo.alonso <pablo.alomen.ucu@gmail.com>
2025-06-06 01:15:54 -0300

    Digilent beginners tutorials: project 1

---
+++ Digilent/project_1_Use_Switches_to_Control_LEDs/led sw.srcs/sources_1/new/led_sw.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/27/2025 01:56:15 AM
+// Design Name: 
+// Module Name: led_sw
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module led_sw(
+    output led,
+    input sw
+);
+ 
+assign led = sw;
+endmodule
